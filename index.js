90016580ba1800133c2ff332c80923af7bfb4b32
sharifulislam <shariful0401@gmail.com>
2022-05-22 21:59:54 +0600

    added admin role

--- index.js
+++ index.js
@@ -46,10 +46,38 @@ async function run() {
       res.send(services);
     });
 
-    app.get('/user', verifyJWT, async(req,res) =>{
+    app.get('/user', verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
+    });
+
+    app.get('/admin/:email', async (req, res) =>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin : isAdmin})
     })
+
+
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester })
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+
+        res.send(result)
+      }
+      else{
+        res.status(403).send({message:'forbiden'})
+      }
+
+
+    });
 
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
@@ -113,8 +141,8 @@ async function run() {
         const bookings = await bookingCollection.find().toArray();
         return res.send(bookings);
       }
-      else{
-        return res.status(403).send({message: 'Forbiden access'})
+      else {
+        return res.status(403).send({ message: 'Forbiden access' })
       }
 
     })
