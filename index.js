4b4ecac2ef4c6d771769ebac435a0a9dfb630a93
sharifulislam <shariful0401@gmail.com>
2022-06-29 21:53:05 +0600

    payment

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
+require('dotenv').config();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const res = require('express/lib/response');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express()
-require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
@@ -40,6 +41,7 @@ async function run() {
     const bookingCollection = client.db('doctors_portal').collection('bookings');
     const userCollection = client.db('doctors_portal').collection('users');
     const doctorCollection = client.db('doctors_portal').collection('doctors');
+    const paymentCollection = client.db('doctors_portal').collection('payments');
 
 
 
@@ -53,7 +55,19 @@ async function run() {
         res.status(403).send({ message: 'forbidden' })
       }
     }
-
+     
+    // Payment api 
+    app.post('/create-payment-intent', verifyJWT, async (req,res) =>{
+      const service = req.body;
+      const price = service.price;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types:['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret})
+    })
 
     app.get('/service', async (req, res) => {
       const query = {};
@@ -153,6 +167,13 @@ async function run() {
 
     })
 
+    app.get('/booking/:id', verifyJWT, async (req, res) =>{
+      const id =req.params.id;
+      const query = {_id: ObjectId(id)};
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking);
+    })
+
     app.post('/booking', async (req, res) => {
       const booking = req.body;
       const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
@@ -164,6 +185,20 @@ async function run() {
       return res.send({ success: true, result });
     });
 
+    app.patch('/booking/:id', verifyJWT, async (req,res) =>{
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = {_id: ObjectId(id)};
+      const updateDoc = {
+        $set:{
+          paid: true,
+          transactionId: payment.transactionId
+        }
+      }
+      const result = await paymentCollection.insertOne(payment);
+      const updatedBooking = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(doctors)
+    })
 
     app.get('/doctor', verifyJWT,verifyAdmin ,async (req,res) =>{
       const doctors = await doctorCollection.find().toArray();
